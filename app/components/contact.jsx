aaa7e8c9600b9b6657242a7e7f3f24a8cc0a2eb5
brunors1219 <brunors1219@gamil.com>
2024-10-09 19:46:49 -0300

    alt emailjs

--- app/components/contact.jsx
+++ app/components/contact.jsx
@@ -1,4 +1,50 @@
+'use client';
+
+import { useState } from "react";
+import  emailjs  from "@emailjs/browser";
+
+
+
 const Contact=()=>{
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [assunto, setAssunto] = useState("");
+
+ 
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        window.location.reload(); // Recarrega a página inteira
+        const serviceId ='service_bzoikxb';
+        const templatId= 'template_jq0ywxl';
+        const publicKey ='Kr78XsfERtr148GXd';
+
+        const templateParams={
+            to_name:"Detetive",
+            from_name: name,
+            from_email: email,
+            client_assunto: assunto,
+            client_message: message,
+        }
+
+        emailjs.send(serviceId, templatId, templateParams,publicKey)
+
+        .they((response) => {
+            console.log('Email enviado com sucesso', response);
+            alert('email enviado com sucesso');
+            setName('');
+            setEmail('');
+            setMessage('');
+            setAssunto('');
+        })
+        .catch((error) => {
+            console.error('erro', error);
+        });
+    }
+
     return( 
         <div id='contato' className="grid justify-items-center grid-cols-1 md:grid-cols-1 p-7 ">
             <div>
@@ -11,6 +57,7 @@ const Contact=()=>{
             <div className="w-full md:w-[400px] mx-auto bg-gray-300 rounded-xl my-6 py-6 font-poppins">
                 <h1 className=" text-xl font-bold text-center">Envia-nos um email</h1>
                 <form
+                    onSubmit={handleSubmit}
                     className="py-4"
                 >
                     <div className="w-[80%] mx-auto py-3">
@@ -20,8 +67,8 @@ const Contact=()=>{
                             placeholder="Digite seu nome"
                             className="w-full border py-1 pl-2 rounded-xl"
                             required
-                            //value={name}
-                            //onChange={(e) => setName(e.target.value)}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="w-[80%] mx-auto py-3">
@@ -31,8 +78,8 @@ const Contact=()=>{
                             placeholder="Digite seu Email"
                             className="w-full border py-1 pl-2 rounded-xl"
                             required
-                            //value={email}
-                            //onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
 
@@ -42,9 +89,8 @@ const Contact=()=>{
                             type="text"
                             placeholder="Assunto"
                             className="w-full border py-1 pl-2 rounded-xl"
-                            required
-                            //value={email}
-                            //onChange={(e) => setEmail(e.target.value)}
+                            value={assunto}
+                            onChange={(e) => setAssunto(e.target.value)}
                         />
                     </div>
                 
@@ -54,14 +100,15 @@ const Contact=()=>{
                             placeholder="Texto"
                             className="w-full border pl-2 rounded-xl"
                             required
-                            //value={message}
-                            //onChange={(e) => setMessage(e.target.value)}
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
                         />
                     </div>
                     <div className="w-[80%] mx-auto py-3">
                         <input
                             type="submit"
-                            className="bg-renata-yellow  transform transition duration-200 ease-in text-xl text-black bg-white  rounded-lg font-semibold uppercase w-full py-3 hover:opacity-5"
+                            
+                            className="transform transition duration-200 ease-in text-xl text-black bg-white  rounded-lg font-semibold uppercase w-full py-3 hover:opacity-5"
                         />
                     </div>
                 </form>
